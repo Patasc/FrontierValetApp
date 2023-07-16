9bc2b585597175c15456953f104fed486413354c
Patasc <45915005+Patasc@users.noreply.github.com>
2023-07-16 18:47:53 +0200

    Made fines form work.

--- src/Components/FinesForm.js
+++ src/Components/FinesForm.js
@@ -6,16 +6,39 @@ export default class FinesForm extends Component{
 
         this.onFormSubmit = this.onFormSubmit.bind(this);
 
+        this.onBaseFineChange = this.onBaseFineChange.bind(this);
+        this.onMinimumFineChange = this.onMinimumFineChange.bind(this);
+        this.onFineIncrementChange = this.onFineIncrementChange.bind(this);
+        this.onFineIntervalChange = this.onFineIntervalChange.bind(this);
+
         this.state = {
-            baseFine: "",
-            minimumFine: "",
-            fineIncrement: "",
-            fineInterval: ""
+            baseFine: 0,
+            minimumFine: 0,
+            fineIncrement: 0,
+            fineInterval: 0
         }
+    }
+
+    onBaseFineChange(e){
+        this.setState({baseFine: e.target.value});
+    }
+
+    onMinimumFineChange(e){
+        this.setState({minimumFine: e.target.value});
+    }
+
+    onFineIncrementChange(e){
+        this.setState({fineIncrement: e.target.value});
+    }
+
+    onFineIntervalChange(e){
+        this.setState({fineInterval: e.target.value});
     }
 
     onFormSubmit(e){
         e.preventDefault();
+
+        localStorage.setItem("fine", JSON.stringify(this.state));
     }
 
     render(){
@@ -25,28 +48,28 @@ export default class FinesForm extends Component{
                     <div style={{overflow: "hidden"}}>
                         <div className="form-group" style={{float: "left", width: "50%"}}>
                             <label>Base Fine</label>
-                            <input required type="number" className="form-control" defaultValue={0}/>
+                            <input required type="number" className="form-control" value={this.state.baseFine} onChange={this.onBaseFineChange}/>
                         </div>
                         <div className="form-group" style={{float: "left", width: "50%"}}>
                             <label>Minimum Fine</label>
-                            <input required type="number" className="form-control" defaultValue={0}/>
+                            <input required type="number" className="form-control" value={this.state.minimumFine} onChange={this.onMinimumFineChange}/>
                         </div>
                     </div>
                     <div style={{overflow: "hidden"}}>
                         <div className="form-group" style={{float: "left", width: "50%"}}>
                             <label>Fine Increment</label>
-                            <input required type="number"  className="form-control" defaultValue={0}/>
+                            <input required type="number"  className="form-control" value={this.state.fineIncrement} onChange={this.onFineIncrementChange}/>
                         </div>
                         <div className="form-group" style={{float: "left", width: "50%"}}>
                             <label>Fine interval</label>
-                            <input required type="number" className="form-control" defaultValue={0}/>
+                            <input required type="number" className="form-control" value={this.state.fineInterval} onChange={this.onFineIntervalChange}/>
                         </div>
                     </div>
 
                     <br/>
 
                     <div className="form-group">
-                        <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                        <button type="submit" className="btn btn-primary btn-block" onSubmit={this.onFormSubmit}>Submit</button>
                     </div>
                 </form>
             </div>
